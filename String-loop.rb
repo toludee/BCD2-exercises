421edde7a603503c701e76ada5afd16b305e22da
toludee <toluwanidaramola@yahoo.com>
2016-02-16 14:02:19 +0100

    3rd Version

--- String-loop.rb
+++ String-loop.rb
@@ -1,6 +1,16 @@
 puts "Username"
 text = gets.chomp
-text.each_char do |text|
-  puts text.upcase!
+#text.each_char do |text|
+  #puts text.upcase!
+
+#puts ("Please input the string: ")
+#input_string = gets.chomp
+
+#Iterates over the characters in the string
+text.each_char do |i|
+   
+   puts "#{text.index(i) +1} #{i.upcase}"
 end
+
+
 
